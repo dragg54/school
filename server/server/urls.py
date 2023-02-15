034ba409280f790d734d2022258597235410fcc7
Ajibola Sadiq <ajibola.sadiq@cognitivelabs.co>
2023-02-15 06:48:04 +0100

    get students

--- server/server/urls.py
+++ server/server/urls.py
@@ -6,7 +6,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/student/', include("student.urls"), name="student"),
-    path('api/v1/payment/', include('payment.urls'), name='payment')
+    path('api/v1/payment/', include('payment.urls'), name='payment'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
